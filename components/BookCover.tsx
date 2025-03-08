146d4a87a716a1c5e87db6042ce78b228fa1e91a
Zack Hanni <zack.hannI@gmail.com>
2025-03-08 12:54:52 -0500

    rendered book cover svg to make image look like a book

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import BookCoverSvg from "./BookCoverSvg";
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
@@ -33,7 +34,7 @@ const BookCover = ({
         className
       )}
     >
-      BOOK SIDE SVG
+      <BookCoverSvg coverColor={coverColor} />
       <div
         className="absolute z-10"
         style={{ left: "12%", width: "87.5%", height: "88%" }}
