9358afee768bf6170c0e8531b2f4d185861c28ec
Zack Hanni <zack.hannI@gmail.com>
2025-03-08 12:46:09 -0500

    fixed name scheme

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React from "react";
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
@@ -15,23 +16,35 @@ interface Props {
   className?: string;
   variant?: BookCoverVariant;
   coverColor: string;
-  coverUrl: string;
+  coverImage: string;
 }
 
 const BookCover = ({
   className,
   variant = "regular",
   coverColor = "#012B48",
-  coverUrl = "https://placeholder.co/400x600.png",
+  coverImage = "https://placehold.co/400x600.png",
 }: Props) => {
   return (
     <div
       className={cn(
         "relative transition-all duration-300",
-        variantStyles[variant]
+        variantStyles[variant],
+        className
       )}
     >
-      BookCover
+      BOOK SIDE SVG
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <Image
+          src={coverImage}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+        />
+      </div>
     </div>
   );
 };
