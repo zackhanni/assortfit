15ff588706453cd429181a85cfca2f21195e468d
Zack Hanni <zack.hannI@gmail.com>
2025-03-08 13:21:41 -0500

    finished book list and latest books section

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import BookCard from "./BookCard";
 
-const BookList = () => {
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: string;
+}
+
+const BookList = ({ title, books, containerClassName }: Props) => {
   return (
-    <section>
-      <h2 className="font-bebas-neue text-4xl text-white">Popular books</h2>
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-4xl text-white">{title}</h2>
+      <ul className="mt-10 flex flex-wrap gap-5 max-xs:justify-between xs:gap-10">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
