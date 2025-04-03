ece76b2ea0db3b5908c12350c05fa79d7a824969
Zack Hanni <zack.hannI@gmail.com>
2025-04-03 16:23:32 -0400

    created generic auth sign in/up pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="relative flex flex-col-reverse text-light-100 sm:flex-row">
+      <section className="my-auto flex h-full min-h-screen flex-1 items-center bg-pattern bg-cover bg-top bg-dark-100 px-5 py-10">
+        <div className="gradient-vertical mx-auto flex max-w-xl flex-col gap-6 rounded-lg p-10 bg-linear-to-r from-[#12141d] to-[#12151f]">
+          <div className="flex flex-row gap-3">
+            <Image src={"/icons/logo.svg"} alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">AssortFit</h1>
+          </div>
+
+          <div>{children}</div>
+        </div>
+      </section>
+
+      <section className="sticky h-40 w-full sm:top-0 sm:h-screen sm:flex-1">
+        <Image
+          src={"/images/auth-illustration.png"}
+          alt="auth illustration"
+          height={1000}
+          width={1000}
+          className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Sign In Form</div>;
+};
+
+export default Page;
