4101cb2dc16bfa1c5ec925044df9ac0f8aba2cb2
Zack Hanni <zack.hannI@gmail.com>
2025-04-16 12:59:36 -0400

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => {
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+
   return (
     <>
       <BookOverview {...sampleBooks[0]} />
