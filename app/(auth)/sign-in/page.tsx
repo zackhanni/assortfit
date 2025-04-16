ffdb0cf79c80a4574053e5db7ae2ae1419808252
Zack Hanni <zack.hannI@gmail.com>
2025-04-16 14:05:01 -0400

    created db logic for signUp and signin with credentials

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthForm from "@/components/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth";
 import { signInSchema } from "@/lib/validations";
 import React from "react";
 
@@ -12,7 +13,7 @@ const Page = () => (
       email: "",
       password: "",
     }}
-    onSubmit={() => {}}
+    onSubmit={signInWithCredentials}
   />
 );
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthForm from "@/components/AuthForm";
+import { signUp } from "@/lib/actions/auth";
 import { signUpSchema } from "@/lib/validations";
 import React from "react";
 
@@ -12,10 +13,10 @@ const Page = () => (
       email: "",
       password: "",
       fullName: "",
-      universityId: "",
+      universityId: 0,
       universityCard: "",
     }}
-    onSubmit={() => {}}
+    onSubmit={signUp}
   />
 );
 
