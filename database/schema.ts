277c95b0d6f1330eab70301f6ff657e2f24b2174
Zack Hanni <zack.hannI@gmail.com>
2025-04-16 17:32:48 -0400

    added demo data to schema - to fix import error in schema

--- database/schema.ts
+++ database/schema.ts
@@ -107,3 +107,37 @@ export const clothing = pgTable("clothing", {
 // SELECT * FROM clothing WHERE 'RED' = ANY(colors);
 // or
 // SELECT * FROM clothing WHERE colors && ARRAY['RED', 'BLACK'];
+
+// delete or edit below after finishing demo
+export const books = pgTable("books", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  title: varchar("title", { length: 255 }).notNull(),
+  author: varchar("author", { length: 255 }).notNull(),
+  genre: text("genre").notNull(),
+  rating: integer("rating").notNull(),
+  coverUrl: text("cover_url").notNull(),
+  coverColor: varchar("cover_color", { length: 7 }).notNull(),
+  description: text("description").notNull(),
+  totalCopies: integer("total_copies").notNull().default(1),
+  availableCopies: integer("available_copies").notNull().default(0),
+  videoUrl: text("video_url").notNull(),
+  summary: varchar("summary").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
+
+export const borrowRecords = pgTable("borrow_records", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  userId: uuid("user_id")
+    .references(() => users.id)
+    .notNull(),
+  bookId: uuid("book_id")
+    .references(() => books.id)
+    .notNull(),
+  borrowDate: timestamp("borrow_date", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  dueDate: date("due_date").notNull(),
+  returnDate: date("return_date"),
+  status: BORROW_STATUS_ENUM("status").default("BORROWED").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
