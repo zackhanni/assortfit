a0bc1d0afb9cbe0198e193021d11d3d24995d14f
Zack Hanni <zack.hannI@gmail.com>
2025-04-17 12:29:58 -0400

    replaced auth image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <main className="relative flex flex-col-reverse text-light-100 sm:flex-row">
-      <section className="my-auto flex h-full min-h-screen flex-1 items-center bg-pattern bg-cover bg-top bg-dark-100 px-5 py-10">
+      <section className="my-auto flex h-full min-h-screen flex-1 items-start sm:items-center bg-pattern bg-cover bg-top bg-dark-100 px-5 py-10">
         <div className="gradient-vertical mx-auto flex max-w-xl flex-col gap-6 rounded-lg p-10 bg-linear-to-r from-[#12141d] to-[#12151f]">
           <div className="flex flex-row gap-3">
             <Image src={"/icons/logo.svg"} alt="logo" width={37} height={37} />
@@ -23,11 +23,12 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
       <section className="sticky h-40 w-full sm:top-0 sm:h-screen sm:flex-1">
         <Image
-          src={"/images/auth-illustration.png"}
+          // src={"/images/auth-illustration.png"}
+          src={"/images/woman-sitting-fashion.jpg"}
           alt="auth illustration"
           height={1000}
           width={1000}
-          className="size-full object-cover"
+          className="size-full object-cover object-top"
         />
       </section>
     </main>
